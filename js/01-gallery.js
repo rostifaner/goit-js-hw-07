d16da3e6f5d4593434ae4f759f71e90ac0c27338
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-13 03:18:36 +0200

    Add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+gallery.addEventListener("click", handleEvent);
+function createMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item"> <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a>
+        </li>`
+    )
+    .join("");
+}
+function handleEvent(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  event.preventDefault();
+  const product = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${product}">`);
+  instance.show();
+}
