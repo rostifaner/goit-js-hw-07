12cdb04002eae01522f08a9ecad94a1059018c84
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-14 22:20:22 +0200

    Add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const createMarkup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          ></img>
+        </a>
+      </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", createMarkup);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+//   closeText: "Close",
+//   download: "Download",
+//   navText: ["prev", "next"],
+//   overlayOpacity: 0.85,
+// });
+// const handleEvent = (event) => {
+//   if (event.target === event.currentTarget) {
+//     return;
+//   }
+//   event.preventDefault();
+//       const sizeBtn = document.querySelectorAll("button");
+//   sizeBtn.forEach((button) => {
+//     button.style.fontSize = "20px";
+//   });
+//   const colorBtn = document.querySelector(".sl-download a");
+//   colorBtn.style.color = "black";
+//   lightbox.open();
+// };
 
-console.log(galleryItems);
+// gallery.addEventListener("click", handleEvent);
